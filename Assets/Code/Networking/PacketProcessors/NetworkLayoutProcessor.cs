5574cced430ffe7cd4a6dca1869a70421b0e20a8
Jack riddell <riddellriddell@gmail.com>
2019-11-23 16:30:53 +1000

    cleaned up code

--- Assets/Code/Networking/PacketProcessors/NetworkLayoutProcessor.cs
+++ Assets/Code/Networking/PacketProcessors/NetworkLayoutProcessor.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Networking
 {
@@ -18,7 +16,7 @@ namespace Networking
                 public long m_lConnectionID;
                 public DateTime m_dtmTimeOfConnection;
 
-                public Connection(long lID,DateTime dtmTimeOfConnection)
+                public Connection(long lID, DateTime dtmTimeOfConnection)
                 {
                     m_lConnectionID = lID;
                     m_dtmTimeOfConnection = dtmTimeOfConnection;
@@ -27,23 +25,23 @@ namespace Networking
             }
 
             public List<Connection> m_conConnectionDetails;
-            
+
             public NetworkLayout(int iConnectionCount)
             {
                 m_conConnectionDetails = new List<Connection>(iConnectionCount);
             }
 
-            public void Add(long lConnectionID,DateTime dtmTimeOfConnection)
+            public void Add(long lConnectionID, DateTime dtmTimeOfConnection)
             {
-                if(m_conConnectionDetails == null)
+                if (m_conConnectionDetails == null)
                 {
                     m_conConnectionDetails = new List<Connection>();
                 }
 
                 //check if it has already been added 
-                for(int i = 0; i < m_conConnectionDetails.Count; i++)
+                for (int i = 0; i < m_conConnectionDetails.Count; i++)
                 {
-                    if(m_conConnectionDetails[i].m_lConnectionID == lConnectionID )
+                    if (m_conConnectionDetails[i].m_lConnectionID == lConnectionID)
                     {
                         m_conConnectionDetails[i] = new Connection(lConnectionID, dtmTimeOfConnection);
                         return;
@@ -63,12 +61,12 @@ namespace Networking
         }
 
         protected NetworkConnection m_ncnNetworkConnection;
-               
+
         public override void OnAddToNetwork(NetworkConnection ncnNetwork)
         {
             m_ncnNetworkConnection = ncnNetwork;
 
-            for(int i = 0; i < m_ncnNetworkConnection.m_conConnectionList.Count; i++)
+            for (int i = 0; i < m_ncnNetworkConnection.m_conConnectionList.Count; i++)
             {
                 OnNewConnection(m_ncnNetworkConnection.m_conConnectionList[i]);
             }
@@ -79,10 +77,10 @@ namespace Networking
             TimeNetworkProcessor tnpNetworkTime = m_ncnNetworkConnection.GetPacketProcessor<TimeNetworkProcessor>();
 
             //create new network layouy tracker
-            ConnectionNetworkLayoutProcessor cnlConnectionLayout = new ConnectionNetworkLayoutProcessor(tnpNetworkTime.NetworkTime,this);
+            ConnectionNetworkLayoutProcessor cnlConnectionLayout = new ConnectionNetworkLayoutProcessor(tnpNetworkTime.NetworkTime, this, conConnection);
 
             conConnection.AddPacketProcessor(cnlConnectionLayout);
-            
+
             //send out updated network layout accross the network 
             base.OnNewConnection(conConnection);
 
@@ -134,7 +132,7 @@ namespace Networking
 
         protected NetworkLayoutProcessor m_nlpNetworkProcessor;
 
-        public ConnectionNetworkLayoutProcessor(DateTime dtmTimeOfCreation, NetworkLayoutProcessor nlpNetworkProcessor,Connection conConnection)
+        public ConnectionNetworkLayoutProcessor(DateTime dtmTimeOfCreation, NetworkLayoutProcessor nlpNetworkProcessor, Connection conConnection)
         {
             m_dtmTimeOfConnection = dtmTimeOfCreation;
             m_nlpNetworkProcessor = nlpNetworkProcessor;
@@ -143,7 +141,7 @@ namespace Networking
 
         public override DataPacket ProcessReceivedPacket(Connection conConnection, DataPacket pktInputPacket)
         {
-            if(pktInputPacket is NetworkLayoutPacket)
+            if (pktInputPacket is NetworkLayoutPacket)
             {
                 m_nlaNetworkLayout = (pktInputPacket as NetworkLayoutPacket).m_nlaNetworkLayout;
 
