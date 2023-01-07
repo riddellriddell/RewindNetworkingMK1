8f8427a69cc35b04e88e14a93562fca6bc22fc00
Jack Riddell <riddellriddell@gmail.com>
2023-01-08 10:27:05 +1100

    - added interfaces to expose data from the sim manager to testing and debugging tools

---
+++ Assets/Code/SharedTypes/ILocalPeerProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTypes
+{
+    public interface ILocalPeerProvider
+    {
+        // get the local peer, returns long.max if there is no local peer
+        long GetLocalPeerID();
+    }
+}
